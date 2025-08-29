46271c6e843aad2bc6aceb02fb4dfcb5a8457bed
Prodip Hore <prodiphore2005@gmail.com>
2025-08-29 13:46:56 +0600

    complete the website

--- main.js
+++ main.js
@@ -18,10 +18,13 @@ function callF(coinIn){
 let coinCount = parseInt(document.getElementById(coinIn).innerText);
     if (coinCount < 20) {
         alert("Can not make a call")
-        return;
+        return 0;
     }
-    coinCount -= 20;
+    
+        coinCount -= 20;
     document.getElementById(coinIn).innerText = coinCount;
+    return 1;
+    
 
 }
 
@@ -48,7 +51,7 @@ function alertF(alertIName,alertINum,callContainer){
    for (const call of callData) {
     const div = document.createElement("div");
     div.innerHTML = `
-    <div class="flex justify-between items-center mx-6 mt-4 mb-4">
+    <div class="flex justify-between items-center mx-1 mt-4 mb-4 p-4 rounded-lg bg-[#fafafa] ">
         <section>
             <p class="font-semibold text-lg break-words max-w-[200px]">${call.name}</p>
             <p class="text-[#5C5C5C] text-lg">${call.number}</p>
@@ -61,14 +64,6 @@ function alertF(alertIName,alertINum,callContainer){
 }
 
 
-
-// //CALLER HISTORY
-// function callHis(){
-
-//     const serviceName = document.getElementById("service-name").innerText;
-//     const serviceNumber = document.getElementById("service-number").innerText;
-    
-// }
 
 
 /*-------------------------------------------------------------------------------- 
@@ -156,11 +151,11 @@ document.getElementById("call-btn1").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name1","service-number1","call-container")
+         
+    
+         if(callF("coin-count") === 1){  // coin যথেষ্ট হলে
+        alertF("service-name1","service-number1","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -170,11 +165,9 @@ document.getElementById("call-btn2").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name2","service-number2","call-container")
+            if(callF("coin-count") === 1){
+            alertF("service-name2","service-number2","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -185,11 +178,9 @@ document.getElementById("call-btn3").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name3","service-number3","call-container")
+    if(callF("coin-count") === 1){
+            alertF("service-name3","service-number3","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -200,11 +191,9 @@ document.getElementById("call-btn4").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name4","service-number4","call-container")
+    if(callF("coin-count") === 1){
+            alertF("service-name4","service-number4","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -215,11 +204,9 @@ document.getElementById("call-btn5").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name5","service-number5","call-container")
+    if(callF("coin-count") === 1){
+            alertF("service-name5","service-number5","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -230,11 +217,9 @@ document.getElementById("call-btn6").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name6","service-number6","call-container")
+    if(callF("coin-count") === 1){
+            alertF("service-name6","service-number6","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -245,11 +230,9 @@ document.getElementById("call-btn7").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name7","service-number7","call-container")
+    if(callF("coin-count") === 1){
+            alertF("service-name7","service-number7","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -261,11 +244,9 @@ document.getElementById("call-btn8").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name8","service-number8","call-container")
+    if(callF("coin-count") === 1){
+            alertF("service-name8","service-number8","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -278,11 +259,9 @@ document.getElementById("call-btn9").addEventListener("click", function(){
     
                          //COIN CUT AND CALLER HISTORY
     //------------------------------------------------------------------------------
-    callF("coin-count")
-    //-------------------------------------------------------------------------------
-                         //ALERT
-    //------------------------------------------------------------------------------
-    alertF("service-name9","service-number9","call-container")
+    if(callF("coin-count") === 1){
+            alertF("service-name9","service-number9","call-container")
+    }
     //----------------------------------------------------------------------------
 
 
@@ -291,4 +270,43 @@ document.getElementById("call-btn9").addEventListener("click", function(){
 
 
 
+/*-------------------------------------------------------------------------------- 
+                                     CLEAR BUTTON
+----------------------------------------------------------------------------------*/
 
+
+document.getElementById("clr-btn").addEventListener("click",function(){
+    callData.length=0;
+    document.getElementById("call-container").innerHTML="";
+})
+
+
+
+
+/*-------------------------------------------------------------------------------- 
+                                     COPY BUTTON
+----------------------------------------------------------------------------------*/
+
+document.querySelectorAll('.copy-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        
+        const card = button.closest('.bg-white');
+        
+        const serviceNumberEl = card.querySelector('h1[id^="service-number"]');
+        const serviceNumber = serviceNumberEl ? serviceNumberEl.innerText.trim() : '';
+
+        if (!serviceNumber) {
+            alert("No number found to copy!");
+            return;
+        }
+
+        
+        navigator.clipboard.writeText(serviceNumber)
+            .then(() => {
+                alert(`Copied: ${serviceNumber}`);
+            })
+            .catch(err => {
+                console.error("Failed to copy", err);
+            });
+    });
+});
